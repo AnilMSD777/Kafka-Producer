1bf5eb6dd590f980d901de6a19043b1b33fbd678
Anil <anilkumarkota1998@gmail.com>
2026-07-05 17:43:51 +0530

    configuring topic bean to have more partitions

--- src/main/java/com/demo/producer/ProducerApp/config/ProducerConfigs.java
+++ src/main/java/com/demo/producer/ProducerApp/config/ProducerConfigs.java
@@ -1,12 +1,14 @@
 package com.demo.producer.ProducerApp.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class ProducerConfigs {
+    @Bean
     public NewTopic newTopic(){
-        return TopicBuilder.name("test1-topic").build();
+        return TopicBuilder.name("test1-topic").partitions(3).replicas(1).build();
     }
 }
